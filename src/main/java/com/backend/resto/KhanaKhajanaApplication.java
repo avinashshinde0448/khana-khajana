08c4a32783a6e2a097dcdf5b190c7a264123ae98
avinashshinde0448 <avishinde0448@gmail.com>
2025-08-08 22:49:46 +0530

    Initial commit

---
+++ src/main/java/com/backend/resto/KhanaKhajanaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.resto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhanaKhajanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhanaKhajanaApplication.class, args);
+	}
+
+}
